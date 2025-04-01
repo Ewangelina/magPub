a66d388ab3cd7985926d2cfc200ef6e723ac71dc
Marta <chadala.marta5a@op.pl>
2025-04-01 21:48:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,113 +1,77 @@
-if False:
-    from kan import KAN
-    import matplotlib.pyplot as plt
-    from sklearn.datasets import make_moons
-    import torch
-    import numpy as np
-    from kan import utils as ku
+import sklearn
+from kan import *
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import load_breast_cancer, load_wine, load_iris
 
-    dtype = torch.get_default_dtype()
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+PATIENCE_VALUE = 3
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+dtype = torch.get_default_dtype()
+result_type = torch.long
+
+def processDataset(data): #sklearn.datasets data
+    y = data.target
+    X = data.data
+    X = sklearn.preprocessing.normalize(X)
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.33) #random_state=7
     dataset = {}
-    train_input, train_label = make_moons(n_samples=1000, shuffle=True, noise=0.1, random_state=None)
-    test_input, test_label = make_moons(n_samples=1000, shuffle=True, noise=0.1, random_state=None)
+    dataset['train_input'] = torch.from_numpy(np.array(X_train)).type(dtype).to(device)
+    dataset['test_input'] = torch.from_numpy(np.array(X_test)).type(dtype).to(device)
+    dataset['train_label'] = torch.from_numpy(np.array(y_train)).type(result_type).to(device)
+    dataset['test_label'] = torch.from_numpy(np.array(y_test)).type(result_type).to(device)
+    return dataset
 
-    dataset['train_input'] = torch.from_numpy(train_input).type(dtype).to(device)
-    dataset['test_input'] = torch.from_numpy(test_input).type(dtype).to(device)
-    dataset['train_label'] = torch.from_numpy(train_label).type(torch.long).to(device)
-    dataset['test_label'] = torch.from_numpy(test_label).type(torch.long).to(device)
-
-    X = dataset['train_input']
-    y = dataset['train_label']
-    plt.scatter(X[:,0].cpu().detach().numpy(), X[:,1].cpu().detach().numpy(), c=y[:].cpu().detach().numpy())
-    model = KAN(width=[2,2], grid=3, k=0, seed=2024, device=device)
-
+def doKAN(dataset, steps, KAN_width, KAN_grid, KAN_degree, KAN_lambda, last_best_acc=0, model=None):
     def train_acc():
         return torch.mean((torch.argmax(model(dataset['train_input']), dim=1) == dataset['train_label']).type(dtype))
 
     def test_acc():
         return torch.mean((torch.argmax(model(dataset['test_input']), dim=1) == dataset['test_label']).type(dtype))
 
-    results = model.fit(dataset, opt="LBFGS", steps=20, metrics=(train_acc, test_acc), loss_fn=torch.nn.CrossEntropyLoss());
-    lib = ['x','x^2','x^3','x^4','exp','log','sqrt','tanh','sin','abs']
-    model.auto_symbolic(lib=lib)
-    coss = model.symbolic_formula()
-    print(coss)
-    formula1, formula2 = coss[0]
+    if model is None:
+        model = KAN(width=KAN_width, grid=KAN_grid, k=KAN_degree, device=device)
 
-    ku.ex_round(formula1, 4)
-    print(formula1)
-    # how accurate is this formula?
-    def acc(formula1, formula2, X, y):
-        batch = X.shape[0]
-        correct = 0
-        for i in range(batch):
-            logit1 = np.array(formula1.subs('x_1', X[i,0]).subs('x_2', X[i,1])).astype(np.float64)
-            logit2 = np.array(formula2.subs('x_1', X[i,0]).subs('x_2', X[i,1])).astype(np.float64)
-            correct += (logit2 > logit1) == y[i]
-        return correct/batch
+    final_results = {}
+    final_results['train_acc'] = []
+    final_results['test_acc'] = []
+    best_test_acc = last_best_acc
+    patience = PATIENCE_VALUE
+    for i in range(steps):
+        fit_results = model.fit(dataset, opt='LBFGS', steps=1, lamb=KAN_lambda, metrics=(train_acc, test_acc), loss_fn=torch.nn.CrossEntropyLoss())
+        final_results['train_acc'].append(fit_results['train_acc'][0])
+        final_results['test_acc'].append(fit_results['test_acc'][0])
+        if fit_results['test_acc'][0] < best_test_acc:
+            if patience == 0:
+                return model, final_results, best_test_acc
+            else:
+                patience -= 1
+        else:
+            patience = PATIENCE_VALUE
+            best_test_acc = fit_results['test_acc'][0]
 
-    print('train acc of the formula:', acc(formula1, formula2, dataset['train_input'], dataset['train_label']))
-    print('test acc of the formula:', acc(formula1, formula2, dataset['test_input'], dataset['test_label']))
-
-
-
-
-    exit(0)
-
-
-
-from kan import *
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.datasets import load_breast_cancer, load_wine, load_iris
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(device)
-dtype = torch.get_default_dtype()
-print(dtype)
+    return model, final_results, best_test_acc
 
 data = load_iris()
-labels = data.target
-exes = data.data
+dataset = processDataset(data)
 
-choice = np.random.choice(range(labels.shape[0]), size=(int(labels.shape[0]*0.8),), replace=False)    
-ind = np.zeros(labels.shape[0], dtype=bool)
-ind[choice] = True
-train_exes = []
-test_exes = []
-train_labels = []
-test_labels = []
-i = 0
-for el in ind:
-    if el:
-        train_labels.append(labels[i])
-        train_exes.append(exes[i])
-    else:
-        test_labels.append(labels[i])
-        test_exes.append(exes[i])
-    i += 1
+#model = KAN(width=[4,4,2,2,3], grid=3, k=0, device=device)
+#results = model.fit(dataset, opt="LBFGS", steps=3, metrics=(train_acc, test_acc), loss_fn=torch.nn.CrossEntropyLoss())
 
-dataset = {}
-dataset['train_input'] = torch.from_numpy(np.array(train_exes)).type(dtype).to(device)
-dataset['test_input'] = torch.from_numpy(np.array(test_exes)).type(dtype).to(device)
-dataset['train_label'] = torch.from_numpy(np.array(train_labels)).type(torch.long).to(device)
-dataset['test_label'] = torch.from_numpy(np.array(test_labels)).type(torch.long).to(device)
+model, results, best_test_acc = doKAN(dataset, 3, [4,4,2,3], 3, 2, 0.001)
 
-model = KAN(width=[4,4,2,2,3], grid=3, k=3, device=device)
+print(results['train_acc'])
+print(results['test_acc'])
 
-def train_acc():
-    return torch.mean((torch.argmax(model(dataset['train_input']), dim=1) == dataset['train_label']).type(dtype))
 
-def test_acc():
-    return torch.mean((torch.argmax(model(dataset['test_input']), dim=1) == dataset['test_label']).type(dtype))
+model, results, best_test_acc = doKAN(dataset, 30, [4,4,2,3], 3, 2, 0.001, last_best_acc=best_test_acc, model=model)
 
-results = model.fit(dataset, opt="LBFGS", steps=3, metrics=(train_acc, test_acc), loss_fn=torch.nn.CrossEntropyLoss())
+print(results['train_acc'])
+print(results['test_acc'])
 
-print(results['train_acc'][-1])
-print(results['test_acc'][-1])
+print(torch.mean((torch.argmax(model(dataset['test_input']), dim=1) == dataset['test_label']).type(dtype)))
 #model.plot(folder='./figures_pre')
+exit(0)
 model.prune()
 model.auto_symbolic()
 model.plot(folder='./figures_post')
