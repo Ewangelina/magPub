5b00cde7ce673be5eeef9c8947ffbd91cb8b27dd
Marta <chadala.marta5a@op.pl>
2025-04-09 21:10:23 +0200

    fix

--- kan loop.py
+++ kan loop.py
@@ -4,11 +4,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from collections import Counter
 from sklearn.datasets import load_breast_cancer, load_wine, load_iris
+import datetime
+
+def string_datetime():
+    return str(datetime.datetime.now()).replace(":", "").replace("-", "").replace(".", "")
 
 PATIENCE_VALUE = 3
 TOLERANCE_AMOUNT = 0
-LOG_FILE_FILEPATH = '.\\text_logs\\' + str(datetime.datetime.now()).replace(":", "").replace("-", "").replace(".", "") + '.txt'
-STEPS = 1
+LOG_FILE_FILEPATH = '.\\text_logs\\' + string_datetime() + '.txt'
+STEPS = 30
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -72,21 +76,24 @@ def write_model_parameters(model):
     line = ""
     act_fun_num = 1
     for fun_part in model.act_fun:
-        line = str(act_fun_num) + ' layer parameters: '
+        line += str(act_fun_num) + ' layer parameters:  '
         for el in fun_part.grid:
-            line += '\n'
+            line = line[:-1] + '\n'
             for elel in el:
                 line += str(elel.item()) + ';'
         line = line[:-1] + '\n'
         act_fun_num += 1
-    write_to_file(line)
+    write_to_file(line[:-1])
 
 def write_model_formula(model):
     line = ""
     formulas = model.symbolic_formula()[0]
     for form in formulas:
         line += str(ex_round(form, 4)) + "\n"
-    write_to_file(line)
+    write_to_file(line[:-1])
+
+
+    
 
 
 data = load_iris()
@@ -115,23 +122,31 @@ for grid in KAN_grid_table:
     for degree in KAN_degree_table:
         for lam in KAN_lambda_table:
             for width in KAN_width_table:
-                intro_line = "------------- start ---- grid: " + str(grid) + " deg: " + str(degree) + " lambda: " + str(lam) + " width: " + str(width)
+                intro_line = "------------- start ---- grid: " + str(grid) + " deg: " + str(degree) + " lambda: " + str(lam) + " width: " + str(width) + " DT: " + string_datetime()
                 write_to_file(intro_line)
                 model, results, best_val_acc, last_i = trainKAN(dataset, steps=STEPS, KAN_width=width, KAN_grid=grid, KAN_degree=degree, KAN_lambda=lam, KAN_sparse_init=False)
-                first_train_line = "normal train: " + str(last_i) + " steps; best val acc: " + str(best_test_acc)
+                first_train_line = "normal train: " + str(last_i) + " steps; best val acc: " + str(best_val_acc)
                 write_to_file(first_train_line)
                 write_model_parameters(model)
 
-                write_to_file("Turning interpretable ------------------")
+                interpretable_line = "Turning interpretable ---" + string_datetime() + "---> formulas for each input:"
+                write_to_file(interpretable_line)
                 model.prune()
                 model.auto_symbolic()
                 write_model_formula(model)
                 new_best_acc = torch.mean((torch.argmax(model(dataset['test_input']), dim=1) == dataset['test_label']).type(dtype)).item()
                 line = "Validation accuracy after turning: " + str(new_best_acc)
                 write_to_file(line)
-                model, results, best_val_acc, last_i = trainKAN(dataset, steps=STEPS, KAN_lambda=lam, last_best_acc=new_best_acc, model=model)
-                second_train_line = "intepretable train: " + str(last_i) + " steps; best val acc: " + str(best_test_acc)
-                write_to_file(second_train_line)
+                
+                train_line = "Training interpretable ---" + string_datetime()
+                write_to_file(train_line)
+                try:
+                    model, results, best_val_acc, last_i = trainKAN(dataset, steps=STEPS, KAN_lambda=lam, last_best_acc=new_best_acc, model=model)
+                    second_train_line = "intepretable train: " + str(last_i) + " steps; best val acc: " + str(best_val_acc) + " DT: " + string_datetime()
+                    write_to_file(second_train_line)
+                except:
+                    fail_line = "FAILED---" + string_datetime()
+                    write_to_file(fail_line)
 
 
                 
